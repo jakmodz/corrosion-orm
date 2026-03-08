2df73eb4ab9bf6e001381abd91067b3b85129922
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-03-08 21:33:30 +0100

    reafactor: Execute Refactor execute to return affected rows

--- crates/cargo-orm-core/src/driver/connection.rs
+++ crates/cargo-orm-core/src/driver/connection.rs
@@ -2,5 +2,6 @@ use crate::error::CargoOrmError;
 
 pub trait Connection: Sized + Sync + Send {
     async fn ping(&self) -> Result<(), CargoOrmError>;
+    async fn execute(&self, query: &str) -> Result<u64, CargoOrmError>;
     fn is_valid(&self) -> bool;
 }
--- crates/cargo-orm-core/src/driver/sql_driver.rs
+++ crates/cargo-orm-core/src/driver/sql_driver.rs
@@ -1,11 +1,23 @@
-use crate::{driver::connection_pool::ConnectionPool, error::CargoOrmError};
+use crate::{
+    driver::{
+        connection::Connection,
+        connection_pool::{ConnectionGuard, ConnectionPool},
+    },
+    error::CargoOrmError,
+};
 
 pub trait SqlDriver: Sized + Sync + Send {
     type Pool: ConnectionPool;
 
     async fn new(config: <Self::Pool as ConnectionPool>::Config) -> Result<Self, CargoOrmError>;
 
-    async fn execute(&self, sql: &str) -> Result<(), CargoOrmError>;
+    async fn execute(&self, sql: &str) -> Result<u64, CargoOrmError> {
+        let guard = self.acquire_conn().await?;
+        guard.execute(sql).await
+    }
+    async fn acquire_conn(&self) -> Result<ConnectionGuard<'_, Self::Pool>, CargoOrmError> {
+        self.pool().acquire_conn().await
+    }
     async fn ping(&self) -> Result<(), CargoOrmError>;
     fn active_connections(&self) -> u32 {
         self.pool().active_conn()
