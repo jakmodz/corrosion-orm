afbdc757dc7f140df9fe33d305e494d0ea3a15be
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-03-08 20:44:13 +0100

    feat: Repository with basic methods

--- crates/cargo-orm-core/src/model/repository.rs
+++ crates/cargo-orm-core/src/model/repository.rs
@@ -1,10 +1,15 @@
-use std::error::Error;
+use crate::{driver::sql_driver::SqlDriver, error::CargoOrmError};
 
 trait Repository: Sized + Sync {
     type PrimaryKey;
+    type Db: SqlDriver;
 
-    async fn save(&self) -> Result<(), Box<dyn Error>>;
-    async fn find() -> Result<Self, Box<dyn Error>>;
-    async fn get_all() -> Result<Vec<Self>, Box<dyn Error>>;
-    async fn get_by_id(id: Self::PrimaryKey) -> Result<Self, Box<dyn Error>>;
+    async fn save<'db>(&self, db: &'db Self::Db) -> Result<(), CargoOrmError>;
+    async fn get_all<'db>(db: &'db Self::Db) -> Result<Vec<Self>, CargoOrmError>;
+    async fn get_by_id<'db>(id: Self::PrimaryKey, db: &'db Self::Db)
+    -> Result<Self, CargoOrmError>;
+    async fn delete_by_id<'db>(
+        id: Self::PrimaryKey,
+        db: &'db Self::Db,
+    ) -> Result<(), CargoOrmError>;
 }
