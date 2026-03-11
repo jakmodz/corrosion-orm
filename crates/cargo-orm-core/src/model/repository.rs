d9212e433f6f7643b2355ae3fd2c7b5b8e90dd69
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-03-11 12:31:53 +0100

    reafactor: Remove unused import and lifetimes that can be elied by compiler

--- crates/cargo-orm-core/src/model/repository.rs
+++ crates/cargo-orm-core/src/model/repository.rs
@@ -1,18 +1,11 @@
-use crate::{
-    driver::{executor::Executor, sql_driver::SqlDriver},
-    error::CargoOrmError,
-};
+use crate::{driver::executor::Executor, error::CargoOrmError};
 
 pub trait Repository: Sized + Sync {
     type PrimaryKey;
     type Db: Executor;
 
-    async fn save<'db>(&self, db: &'db Self::Db) -> Result<Self, CargoOrmError>;
-    async fn get_all<'db>(db: &'db Self::Db) -> Result<Vec<Self>, CargoOrmError>;
-    async fn get_by_id<'db>(id: Self::PrimaryKey, db: &'db Self::Db)
-    -> Result<Self, CargoOrmError>;
-    async fn delete_by_id<'db>(
-        id: Self::PrimaryKey,
-        db: &'db Self::Db,
-    ) -> Result<(), CargoOrmError>;
+    async fn save(&self, db: &Self::Db) -> Result<Self, CargoOrmError>;
+    async fn get_all(db: &Self::Db) -> Result<Vec<Self>, CargoOrmError>;
+    async fn get_by_id(id: Self::PrimaryKey, db: &Self::Db) -> Result<Self, CargoOrmError>;
+    async fn delete_by_id(id: Self::PrimaryKey, db: &Self::Db) -> Result<(), CargoOrmError>;
 }
