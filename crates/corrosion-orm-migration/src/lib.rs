0f1df94a5608646eb3d5ede9af25e3784a0d12c9
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-06-25 18:14:12 +0200

    chore: Remove hardcoded paths, feature based module comiplation for sqlite migration executor

--- crates/corrosion-orm-migration/src/lib.rs
+++ crates/corrosion-orm-migration/src/lib.rs
@@ -5,7 +5,9 @@ pub mod migration;
 pub mod migration_generator;
 pub mod migration_registry;
 pub mod runner;
+#[cfg(feature = "sqlite")]
 pub mod sqlite_executor;
 pub use migration::{MigrationExecutor, MigrationTrait, MigratorTrait};
 pub use runner::MigrationStatus;
+#[cfg(feature = "sqlite")]
 pub use sqlite_executor::SqliteMigrationExecutor;
